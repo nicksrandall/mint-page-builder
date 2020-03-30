b69c831e59102457588cf06bfd2dd984f2864594
Nick Randall <nicksrandall@gmail.com>
2020-03-30 12:36:58 -0700

    add description to page builder component

--- src/index.js
+++ src/index.js
@@ -46,40 +46,47 @@ const bootstrap = sdk => {
     sdk.window.startAutoResizer();
   } else if (sdk.location.is(locations.LOCATION_ENTRY_FIELD)) {
     ReactDOM.render(
-      <Button
-        buttonType="primary"
-        icon="Edit"
-        size="large"
-        onClick={() => {
-          sdk.dialogs
-            .openCurrentApp({
-              title: "Page Builder",
-              width: "fullWidth",
-              minHeight: "960px",
-              shouldCloseOnOverlayClick: true,
-              parameters: { initialValue: sdk.field.getValue() }
-            })
-            .then(data => {
-              console.log("set field", data);
-              if (data) {
-                return sdk.field.setValue(data);
-              }
-            });
-        }}
-      >
-        Open page builder
-      </Button>,
+      <>
+        <p>
+          Click the button below to use the page building tool to configure this
+          page's layout and components. This experience is completely custom and
+          can be adapted to be whatever we want.
+        </p>
+        <Button
+          buttonType="primary"
+          icon="Edit"
+          size="large"
+          onClick={() => {
+            sdk.dialogs
+              .openCurrentApp({
+                title: "Page Builder",
+                width: "fullWidth",
+                minHeight: "960px",
+                shouldCloseOnOverlayClick: true,
+                parameters: { initialValue: sdk.field.getValue() }
+              })
+              .then(data => {
+                console.log("set field", data);
+                if (data) {
+                  return sdk.field.setValue(data);
+                }
+              });
+          }}
+        >
+          Open page builder
+        </Button>
+      </>,
       document.getElementById("root")
     );
   }
 };
 
 if (process.env.NODE_ENV === "production") {
-  console.log('bootstrap from production');
+  console.log("bootstrap from production");
   init(bootstrap);
 } else {
   // fake sdk for local devlopement
-  console.log('bootstrap with sample data');
+  console.log("bootstrap with sample data");
   const noop = () => {};
   class Location {
     constructor(loc) {
