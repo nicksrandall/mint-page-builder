42889132c0b21bb499e8ec819e2c6adf182b4f81
Nick Randall <nicksrandall@gmail.com>
2020-03-31 08:30:03 -0700

    [NR] more app config

--- src/components/AppConfigure.js
+++ src/components/AppConfigure.js
@@ -1,56 +1,61 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Component, Suspense } from "react";
 
 const Editor = React.lazy(() => import("./JSONEditor"));
 
-const AppConfigure = ({ sdk }) => {
-  const [value, setValue] = useState(null);
-
-  useEffect(() => {
-    sdk.app.getParameters().then(params => {
-      setValue(JSON.stringify(params.components, null, "  "));
-      sdk.app.setReady();
-    });
-  }, [sdk.app]);
-
-  useEffect(() => {
-    sdk.app.onConfigure(() => {
-      try {
-        const components = window.jsonlint.parse(value);
-        if (!Array.isArray(components)) {
-          sdk.notifier.error(
-            "JSON must be an array - fix it before installing."
-          );
-          return false;
+class AppConfigure extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: [] };
+    this.props.sdk.app.onConfigure(() => this.onConfigure());
+  }
+  componentDidMount() {
+    // Get current parameters of the app.
+    this.props.sdk.app.getParameters().then(params => {
+      this.setState(
+        { value: JSON.stringify(params.components || [], null, "  ") },
+        () => {
+          this.props.sdk.app.setReady();
         }
-        return {
-          parameters: { components },
-          targetState: {
-            EditorInterface: {
-              page: {
-                controls: [{ fieldId: "layout" }]
-              }
-            }
-          }
-        };
-      } catch {
-        sdk.notifier.error("Invalid JSON - fix it before installing.");
+      );
+    });
+  }
+  onConfigure() {
+    try {
+      console.log("value", this.state.value);
+      const components = window.jsonlint.parse(this.state.value);
+      if (!Array.isArray(components)) {
+        this.props.sdk.notifier.error(
+          "JSON must be an array - fix it before installing."
+        );
         return false;
       }
-    });
-  }, [sdk, value]);
-
-  return (
-    <Suspense fallback={<div>loading...</div>}>
-      <div css={{ height: "100vh", overflow: 'auto' }}>
-        <Editor
-          value={value}
-          onBeforeChange={(editor, data, value) => {
-            setValue(value);
-          }}
-        />
-      </div>
-    </Suspense>
-  );
-};
+      return {
+        parameters: { components },
+        targetState: {
+          EditorInterface: {
+            page: {
+              controls: [{ fieldId: "layout" }]
+            }
+          }
+        }
+      };
+    } catch {
+      this.props.sdk.notifier.error("Invalid JSON - fix it before installing.");
+      return false;
+    }
+  }
+  handleChange = (_editor, _data, value) => {
+    this.setState({ value });
+  };
+  render() {
+    return (
+      <Suspense fallback={<div>loading...</div>}>
+        <div css={{ height: "100vh", overflow: "auto" }}>
+          <Editor value={this.state.value} onBeforeChange={this.handleChange} />
+        </div>
+      </Suspense>
+    );
+  }
+}
 
 export default AppConfigure;
